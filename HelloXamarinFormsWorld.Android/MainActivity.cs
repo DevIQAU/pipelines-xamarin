0d94b9b20e2fead8261aaee90a4faf24380709c8
Richard Angus <richard.angus@live.com.au>
2018-10-13 20:03:12 +1000

    Added AppCenter integration

--- HelloXamarinFormsWorld.Android/MainActivity.cs
+++ HelloXamarinFormsWorld.Android/MainActivity.cs
@@ -4,6 +4,10 @@ using Android.OS;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace HelloXamarinFormsWorld.Droid
 {
     [Activity(Label = "HelloXamarinFormsWorld", MainLauncher = true)]
@@ -11,6 +15,8 @@ namespace HelloXamarinFormsWorld.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
+            AppCenter.Start("2ce106b9-d38f-41c7-8e9f-086b983abaf3", typeof(Analytics), typeof(Crashes));
+
             base.OnCreate(bundle);
 
             Forms.Init(this, bundle);
